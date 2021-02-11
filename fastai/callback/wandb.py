70fdd540fdc6a6b2a51ead261db68880d2fc2949
Fariz Rahman <farizrahman4u@gmail.com>
2021-02-11 15:32:08 +0400

    Update wandb.py

--- fastai/callback/wandb.py
+++ fastai/callback/wandb.py
@@ -137,9 +137,9 @@ def gather_args(self:Learner):
     try:
         n_inp = self.dls.train.n_inp
         args['n_inp'] = n_inp
-        xb = self.dls.train.one_batch()[:n_inp]
-        args.update({f'input {n+1} dim {i+1}':d for n in range(n_inp) for i,d in enumerate(list(detuplify(xb[n]).shape))})
-    except: pass#print(f'Could not gather input dimensions')
+#         xb = self.dls.train.one_batch()[:n_inp]
+#         args.update({f'input {n+1} dim {i+1}':d for n in range(n_inp) for i,d in enumerate(list(detuplify(xb[n]).shape))})
+    except: print(f'Could not gather input dimensions')
     # other useful information
     with ignore_exceptions():
         args['batch size'] = self.dls.bs
